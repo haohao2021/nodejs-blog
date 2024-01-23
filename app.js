9f8b0b094181b01628753571a5e884275318f65d
haohao <haohao.yu@qq.com>
2024-01-23 08:57:08 +0100

    updated

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const blogRoutes = require('./routes/blogRoutes');
 const app = express();
 
 // connect to mongodb & listen for requests
-const dbURI = "mongodb+srv://netninja:test1234@cluster0.zglkpky.mongodb.net/node-tuts?retryWrites=true&w=majority";
+const dbURI = "mongodb+srv://haohaoyu1123:yuhang89890@cluster0.zglkpky.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(result => app.listen(3000))
